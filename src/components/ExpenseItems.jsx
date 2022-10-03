96efef4dd7440b9f1a5824beb9038d79013090f7
GLOBAL\H521816 <lakshmanan.arasakumar@honeywell.com>
2022-10-03 09:53:26 -0400

    Video-36: Dynamic Data

--- src/components/ExpenseItems.jsx
+++ src/components/ExpenseItems.jsx
@@ -1,12 +1,16 @@
 import './ExpenseItems.css';
 
 function ExpenseItem() {
+    const expenseDate = new Date(2022, 10, 3);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>03 Oct 2022</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$294.67</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
